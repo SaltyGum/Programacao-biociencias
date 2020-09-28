cebae5033980f7c0e8198fc616b4ccca97e1aab6
JEFFERSON LUIZ OLIVEIRA SIMOES <70494815+SaltyGum@users.noreply.github.com>
2020-09-27 23:48:13 -0300

    Add files via upload

---
+++ CalculaQuantAbsoluta.py
@@ -0,0 +1,25 @@
+# importando todas as bibliotecas
+import sys
+import xlrd
+import pandas as pd
+import numpy as np
+#Pegando entrada
+entrada = sys.argv[1::]
+arq = entrada[0]
+b = int(entrada[1])
+m = int(entrada[2])
+# Copiando arquivo
+df = pd.read_excel(arq).copy()
+# Passando colunas selecionadas
+df_q = df[['Target_Name', 'Stage']].copy()
+# Criando coluna
+df_q["Quantity"] = 10 ** ((df[('CT')].copy() - b) / m)
+#Juntando colunas
+df_final = pd.merge(df.copy(),df_q.copy())
+#Criando arquivo .xlsx Final
+saida = pd.ExcelWriter(r"FINAL.xlsx")
+df_final.to_excel(saida, 'Aba')
+#Salvando arquivo
+saida.save()
+#Printando Lista final
+print(df_final)
